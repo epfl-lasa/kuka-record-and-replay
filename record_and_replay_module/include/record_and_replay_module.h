9a3d5709b1a223e2bfb3af5eb53fb053bae30229
Klas Kronander <klas.kronander@epfl.ch>
2015-07-23 16:47:38 +0200

    initial commit

---
+++ record_and_replay_module/include/record_and_replay_module.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2010 Learning Algorithms and Systems Laboratory, EPFL, Switzerland
+ * Author: Eric Sauser
+ * email:   eric.sauser@a3.epf.ch
+ * website: lasa.epfl.ch
+ *
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#ifndef record_and_replay_module_H_
+#define record_and_replay_module_H_
+
+#include "RobotLib/RobotInterface.h"
+#include "KUKARobotModel/LWRRobot.h"
+
+#include "RobotLib/Sensor.h"
+#include "RobotLib/KinematicChain.h"
+
+#include <vector>
+#include "MotionGenerators/CDDynamics.h"
+
+
+class record_and_replay_module : public RobotInterface
+{
+    // a vector to store the recorded joint space trajectory
+    vector<MathLib::Vector> trajectory_;
+    // and integer to keep track of where we are at in the recorded trajectory
+    size_t traj_ind_;
+    // just an enum to keep track of what the controller is doing in its main loop
+    enum CurrentState {NONE,RECORD,PREPARE,REPLAY};
+    CurrentState current_state_;
+    // a pointer to the lwr_robot which carries specific kuka functionalities like cartesian or joint space impedance control
+    LWRRobot * lwr_robot_;
+    // an object that will get the values of the joint encoders
+    RevoluteJointSensorGroup joint_sensors_;
+    // an object to which we will be able to give the commands to the robot
+    RevoluteJointActuatorGroup joint_actuators_;
+    // an object that we will use to generate smooth interpolated trajectories to the starting point of the recorded traj
+    CDDynamics * cd_dyn_;
+
+    // just a little bool to keep track of when we start and stop a new state of the robot
+    bool bFirst_;
+
+    // these are two vectors we will use to store stiffness values for gravity compensating and moving respectively
+    MathLib::Vector gravcomp_stiffness_;
+    MathLib::Vector moving_stiffness_;
+
+
+
+
+public:
+            record_and_replay_module();
+    virtual ~record_and_replay_module();
+  
+    virtual Status              RobotInit();
+    virtual Status              RobotFree();
+  
+    virtual Status              RobotStart();    
+    virtual Status              RobotStop();
+  
+    virtual Status              RobotUpdate();
+    virtual Status              RobotUpdateCore();
+
+            virtual int                 RespondToConsoleCommand(const string cmd, const vector<string> &args);
+private:
+            void SwitchState(CurrentState next_state);
+};
+
+
+
+#endif 
---
+++ record_and_replay_module/src/record_and_replay_module.cpp
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2010 Learning Algorithms and Systems Laboratory, EPFL, Switzerland
+ * Author: Eric Sauser
+ * email:   eric.sauser@a3.epf.ch
+ * website: lasa.epfl.ch
+ *
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#include "record_and_replay_module.h"
+
+
+record_and_replay_module::record_and_replay_module()
+    :RobotInterface(){
+}
+record_and_replay_module::~record_and_replay_module(){
+}
+
+RobotInterface::Status record_and_replay_module::RobotInit(){
+    // initializeing the robot. This is a hack to avoid seg faults in case we run on simulation
+    if(mRobot->IsSimulationMode()){
+        lwr_robot_ = new LWRRobot;
+    }else{
+        lwr_robot_ = (LWRRobot*) mRobot;
+        lwr_robot_->SetControlMode(Robot::CTRLMODE_JOINTIMPEDANCE);
+    }
+    // set up the joint sensors
+    joint_sensors_.SetSensorsList(lwr_robot_->GetSensors());
+    // set up the joint actuators
+    joint_actuators_.SetActuatorsList(lwr_robot_->GetActuators());
+
+
+    gravcomp_stiffness_.Resize(7);
+    gravcomp_stiffness_.Zero();
+
+    moving_stiffness_.Resize(7);
+    // lets use the default values from the lwr_robot
+    moving_stiffness_ = lwr_robot_->GetJointStiffness();
+
+    traj_ind_ = 0;
+
+    // finally define some commands that allows to interract with the controller via the console
+    AddConsoleCommand("gravcomp");
+    AddConsoleCommand("record");
+    AddConsoleCommand("stop");
+    AddConsoleCommand("replay");
+    return STATUS_OK;
+}
+RobotInterface::Status record_and_replay_module::RobotFree(){
+    return STATUS_OK;
+}
+RobotInterface::Status record_and_replay_module::RobotStart(){
+    return STATUS_OK;
+}    
+RobotInterface::Status record_and_replay_module::RobotStop(){
+    return STATUS_OK;
+}
+RobotInterface::Status record_and_replay_module::RobotUpdate(){
+    // for the kuka robot, this is redunant with RobotUpdateCore.
+    return STATUS_OK;
+}
+RobotInterface::Status record_and_replay_module::RobotUpdateCore(){
+    // this is the main loop. Every 1/500 second this will be called, and any new control command set to lwr_robot_ will then be applied to the robot
+    // lets start by updating the position sensors
+    joint_sensors_.ReadSensors();
+
+    // now, what we should do here depends on which state we are in..
+    switch(current_state_){
+    {
+    case NONE:
+            if(bFirst_)
+                bFirst_ = false;
+            // here we should just gravity compensate. Therefore, we set zero stiffness
+            lwr_robot_->SetJointStiffness(gravcomp_stiffness_);
+            // also we had better feed back the actual joint positions as targets to avoid annoying low-level kuka errors..
+            joint_actuators_.SetJointPositions(joint_sensors_.GetJointPositions());
+            break;
+    }
+    {
+    case RECORD:
+            // here we should do exactly the same except now we also record the data coming from the sensors
+            if(bFirst_){
+                trajectory_.clear();
+                lwr_robot_->SetJointStiffness(gravcomp_stiffness_);
+                bFirst_ = false;
+            }
+            trajectory_.push_back(joint_sensors_.GetJointPositions());
+            // also we had better feed back the actual joint positions as targets to avoid annoying low-level kuka errors..
+            joint_actuators_.SetJointPositions(joint_sensors_.GetJointPositions());
+            break;
+    }
+    {
+    case PREPARE:
+            // we prepare to replay by moving smoothly to the first configuration in the recorded trajectory
+            if(bFirst_){
+                // set the target configuration for the trajectory interpolator
+                cd_dyn_->SetTarget(trajectory_[0]);
+                // initialize the trajectory interpolator with our current position
+                cd_dyn_->SetState(joint_sensors_.GetJointPositions());
+                // but now we want to move! so we need to have some stiffness..
+                lwr_robot_->SetJointStiffness(moving_stiffness_);
+                bFirst_ = false;
+            }
+            // invoke the internal update of the trajectory interpolator
+            cd_dyn_->Update();
+            // extract the current position from the trajectory interpolator
+            MathLib::Vector desired_position(7);
+            cd_dyn_->GetState(desired_position);
+            // .. and apply it to the robot as desired position command
+            joint_actuators_.SetJointPositions(desired_position);
+            // finally we check if we are close to the starting configuration, in which case we can switch to replaying the trajectory
+            if((joint_sensors_.GetJointPositions() - trajectory_[0]).Norm()<0.001)
+                SwitchState(REPLAY);
+            break;
+    }
+    {
+    case REPLAY:
+            if(bFirst_){
+                traj_ind_ = 0;
+                bFirst_ = false;
+            }
+            // apply the desired positions and increment
+            joint_actuators_.SetJointPositions(trajectory_[traj_ind_]);
+            traj_ind_++;
+            // if we finished replaying, switch back to prepare
+            if(traj_ind_>trajectory_.size()-1){
+                SwitchState(PREPARE);
+            }
+
+            break;
+
+    }
+    }
+
+    joint_actuators_.WriteActuators();
+    return STATUS_OK;
+}
+
+void record_and_replay_module::SwitchState(CurrentState next_state){
+    current_state_ = next_state;
+    bFirst_ = true;
+}
+
+int record_and_replay_module::RespondToConsoleCommand(const string cmd, const vector<string> &args){
+    // here we have to define what happens when the commands defined in the Init method are issued
+    if(cmd=="gravcomp")
+        SwitchState(NONE);
+    else if(cmd=="record")
+        SwitchState(RECORD);
+    else if (cmd=="stop")
+        SwitchState(NONE);
+    else if(cmd=="replay")
+        SwitchState(PREPARE);
+
+
+        return 0;
+}
+
+
+extern "C"{
+// These two "C" functions manage the creation and destruction of the class
+record_and_replay_module* create(){return new record_and_replay_module();}
+void destroy(record_and_replay_module* module){delete module;}
+}
+
